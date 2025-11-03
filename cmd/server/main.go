1e6d1aa829135f4edd3b94fa6c0cbc1373d94786
Emiliano Anichini <emiliano.anichini@gmail.com>
2025-11-03 10:42:42 +0100

    CH2L4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		fmt.Printf("Error on creating connection: %s", err)
+	}
+	fmt.Println("Connection established")
+	// Set up channel on which to send signal notifications.
+	// We must use a buffered channel or risk missing the signal
+	// if we're not ready to receive when the signal is sent.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received.
+	<-c
+	fmt.Println("Got signal")
+	defer func() {
+		fmt.Println("Closing AMQP connection...")
+		if err := conn.Close(); err != nil {
+			fmt.Printf("Error closing connection: %s", err)
+		}
+	}()
 }
