d3376bdee0fd3d8c9a272ce22725962bc01aae0b
Emiliano Anichini <emiliano.anichini@gmail.com>
2025-11-19 15:16:20 +0100

    CH3L1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -14,8 +16,29 @@ func main() {
 	conn, err := amqp.Dial(connStr)
 	if err != nil {
 		fmt.Printf("Error on creating connection: %s", err)
+		return
 	}
 	fmt.Println("Connection established")
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Error on creating connection channel: %s", err)
+		return
+	}
+
+	defer func() {
+		fmt.Println("Closing AMQP connection...")
+		if err := conn.Close(); err != nil {
+			fmt.Printf("Error closing connection: %s", err)
+		}
+	}()
+
+	arg := routing.PlayingState{}
+	arg.IsPaused = true
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, arg)
+	if err != nil {
+		fmt.Printf("Error publishing json: %s", err)
+	}
+
 	// Set up channel on which to send signal notifications.
 	// We must use a buffered channel or risk missing the signal
 	// if we're not ready to receive when the signal is sent.
@@ -25,10 +48,4 @@ func main() {
 	// Block until a signal is received.
 	<-c
 	fmt.Println("Got signal")
-	defer func() {
-		fmt.Println("Closing AMQP connection...")
-		if err := conn.Close(); err != nil {
-			fmt.Printf("Error closing connection: %s", err)
-		}
-	}()
 }
---
+++ internal/pubsub/publishJson.go
@@ -0,0 +1,18 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	b, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+	p := amqp.Publishing{ContentType: "application/json", Body: b}
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, p)
+	return err
+}
